ef3c30ddbda281a42d6c32bd345782b06f8f0500
jack5341 <nedim.akar53411@gmail.com>
2021-06-21 20:26:19 +0300

    seriously first commit

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 import express from "express";
 import cors from "cors"
 import morgan from "morgan";
-import crypto from "crypto"
 const app = express();
-
-// Routes
-import auth from "./routes/auth";
-import services from "./routes/services";
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
-app.use("/a", auth);
-app.use("/s", services);
 
-app.get("/", (req, res) => {
-    res.status(200).send(crypto.randomUUID().fixed())
-});
 
 const PORT = 4000 || process.env.PORT;
 app.listen(PORT);
