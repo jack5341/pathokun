8511473b2ad93688d6e90786eca7d5f76c5cc51c
Nedim <nedim.akar53411@gmail.com>
2021-07-17 14:49:50 +0200

    create endpoint form is complated

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import path from "path"
+import open from "open"
 const app = express();
 dotenv.config();
 
 app.use(cors());
+app.set("view-engine", "ejs")
+app.set("views", path.join(__dirname, "/views"))
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '../public'))
 
 app.get("/", (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Pathokun!</h1>');
-  res.end();
+  res.render("index.ejs")
+  res.status(200).end();
 });
 
 mongoose.connect(
@@ -32,4 +36,5 @@ mongoose.connect(
 );
 
 const PORT = process.env.PORT || 4000;
+open(`http://localhost:${PORT}`);
 app.listen(PORT);
