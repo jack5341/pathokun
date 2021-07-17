77e62c26420d05b966e8f77bf9fa03045c28f091
Nedim <nedim.akar53411@gmail.com>
2021-07-17 18:45:54 +0200

    feat: added createdb script and added to package.json

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.set("views", path.join(__dirname, "/views"))
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '../public'))
+app.use(express.static(__dirname + '../views'))
 
 app.get("/", (req, res) => {
   res.render("index.ejs")
---
+++ utils/createdb.js
@@ -0,0 +1,19 @@
+import fs from "fs-extra";
+import crypto from "crypto";
+import figlet from "figlet";
+import path from "path";
+import inquirer from "inquirer";
+import bcrypt from "bcrypt";
+
+(async() => {
+  console.clear();
+  console.log(`\x1b[34m ${figlet.textSync("Pathokun !", { horizontalLayout: "full" })} \x1b[34m
+\x1b[0m Pathokun, a endpoint generater, update your content just with your frontend by HTTP GET Request!
+`)
+  const answers = await inquirer.prompt({name: "secretkey", type: "password", message: "Enter you SUPER SECRET KEY :"})
+  fs.writeJSON(path.join(__dirname, "..", "master", ".", "db", "db.json"), {
+    uuid: crypto.randomUUID(),
+    secret_key: bcrypt.hashSync(answers.secretkey, 12),
+  });
+  console.log("\n Database created succesfully ! \n")
+})();
