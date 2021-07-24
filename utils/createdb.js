e8b88deeeaaf0504c3c8c84d35194c2764212f80
Nedim Akar <nedim.akar53411@gmail.com>
2021-07-24 23:37:47 +0200

    cleaned

--- utils/createdb.js
+++ utils/createdb.js
@@ -4,10 +4,13 @@ import figlet from "figlet";
 import path from "path";
 import inquirer from "inquirer";
 
+const BLUE = `\x1b[1;34m`;
+const OWN_COLOR = `\x1b[0m`;
+
 (async() => {
   console.clear();
-  console.log(`\x1b[34m ${figlet.textSync("Pathokun !", { horizontalLayout: "full" })} \x1b[34m
-\x1b[0m Pathokun, a endpoint generater, update your content just with your frontend by HTTP GET Request!
+  console.log(`${BLUE} ${figlet.textSync("Pathokun !", { horizontalLayout: "full" })}
+${OWN_COLOR} Pathokun, a endpoint generater, update your content just with your frontend by HTTP GET Request!
 `)
   const answers = await inquirer.prompt({name: "secretkey", type: "password", message: "Enter you SUPER SECRET KEY :"})
   fs.mkdirpSync(path.join(__dirname, "..", "master", ".", "db"))
